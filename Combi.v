38e3554860dc4b5ef5e84ac1747d862a95633c09
Alexandre Moine <alexandre@moine.me>
2020-03-27 10:51:17 +0100

    letouzey

--- Combi.v
+++ Combi.v
@@ -1,58 +1,58 @@
-Require Import Coq.Arith.Arith_base.
+Inductive typ :=
+| Iota
+| Arrow (t1:typ) (t2:typ).
 
-(* Inductive lambda {A : Set} : type -> Set :=
-| Var (t:type) (x:A) : lambda t
-| Lam (t1:type) (x:A) {t2:type} (b:lambda t2) : lambda (Arrow t1 t2)
-| App {t1 t2:type} (x:lambda (Arrow t1 t2)) (y:lambda t1) : lambda t2. *)
+Class Ord (A:Type) := cmp : A -> A -> Prop.
+Infix "<<" := cmp (at level 70, no associativity).
 
-(* Fixpoint tot t : Type :=
-  match t with
-  | Iota => nat
-  | Arrow t1 t2 => (tot t1) -> (tot t2) end. *)
+(* Les quelques constructions d'ordres qui serviront après *)
+Instance ordnat : Ord nat := lt.
+Instance ordfun A B `(Ord B) : Ord (A->B) :=
+ fun f g => forall a, f a << g a.
+Instance ordsig A (P:A->Prop)`(Ord A) : Ord {a:A|P a} :=
+ fun a a' => proj1_sig a << proj1_sig a'.
 
-Definition lt' : {_: nat | True} -> {_:nat | True} -> Prop :=
-  fun x y =>
-  match x,y with
-  | exist _ x _, exist _ y _ => lt x y end.
+Definition Incr {A}{B}`(Ord A)`(Ord B) (f:A->B) :=
+ forall a a', a << a' -> f a << f a'.
 
-Inductive Interp : forall (T:Type) (P:T -> Prop), (sig P -> sig P -> Prop) -> Prop :=
-| Iota : Interp nat (fun _ => True) lt'
-| Arrow {t1 t2} {int1 int2} {c1 c2} (x:Interp t1 int1 c1) (y:Interp t2 int2 c2) :
-    Interp (sig int1 -> sig int2) (fun f => forall v v', c1 v v' -> c2 (f v) (f v'))
-           (fun f g => forall v, c2 (proj1_sig f v) (proj1_sig g v)).
+Record pack := Pack { dom :> Type; ord : Ord dom }.
 
-Definition tot {T P cmp} (t:Interp T P cmp) : Type := T.
-Definition interp {T P cmp} (t:Interp T P cmp) : T -> Prop := P.
-Definition cmp {T P cmp} (t:Interp T P cmp) : sig P -> sig P -> Prop := cmp.
+(* NB: La projection dom peut être implicite (cf le :> ci-dessus)
+   donc un pack peut être utilisé à la place d'un type Coq *)
 
-Require Import Coq.Relations.Relation_Definitions.
+Fixpoint interp (t:typ) : pack :=
+ match t with
+ | Iota => Pack nat _
+ | Arrow t1 t2 =>
+   Pack { f : dom (interp t1) -> dom (interp t2) | Incr (ord (interp t1)) (ord (interp t2)) f }
+        (ordsig _ _ (ordfun  _ _ (ord (interp t2))))
+ end.
 
-Lemma lt'_spec : forall x y, lt' x y <-> lt (proj1_sig x) (proj1_sig y).
+(* Evidemment, le type dans un pack est ordonné *)
+Instance ordpack (p:pack) : Ord p := p.(ord).
+
+Require Import Coq.Arith.Arith.
+Require Import Coq.Classes.RelationClasses.
+
+Instance odrtrans t : Transitive (ordpack (interp t)).
 Proof.
-  intros.
-  destruct x.
-  destruct y.
-  intuition.
-Qed.
-
-Lemma cmp_trans {T P C} (t:Interp T P C) : transitive (sig P) C.
-Proof.
-  unfold transitive.
-  intros.
   induction t.
-  - destruct x. destruct y. destruct z.
-    simpl; simpl in H; simpl in H0.
-    exact (lt_trans x x0 x1 H H0).
-  - intros.
-    apply IHt2 with (y := (proj1_sig y v)).
-    intuition. intuition.
+  - intuition.
+  - unfold ordpack in *.
+    unfold ord.
+    simpl.
+    unfold ordsig.
+    unfold Transitive.
+    unfold cmp.
+    unfold ordfun.
+    intros.
+    unfold Transitive in IHt2.
+    exact (IHt2 (proj1_sig x a) (proj1_sig y a) (proj1_sig z a) (H a) (H0 a)).
 Qed.
 
-Fixpoint plus {T P C} (t:Interp T P C) : sig P -> nat -> sig P :=
-  match t with
-  | Iota => fun x k => exist (fun _ => True) (proj1_sig x + k) I
-  | Arrow x y =>
-    fun f k =>
-      let f' := fun v => plus y (proj1_sig f v) k in
-      sexist (fun f => forall v v', (cmp x) v v' -> cmp y (f v) (f v'))
+Fixpoint plus t : dom (interp t) -> nat -> dom (interp t) :=
+  match t as t return dom (interp t) -> nat -> dom (interp t)  with
+  | Iota => fun x y => x + y
+  | Arrow t1 t2 =>
+    fun f k => exist _ (fun v => plus t2 (proj1_sig f (proj1_sig v)) k)
   end.
