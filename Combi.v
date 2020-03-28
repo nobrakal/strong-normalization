f2f21894f7af0377bc612491ef3272820120e0c5
Alexandre Moine <alexandre@moine.me>
2020-03-28 11:05:31 +0100

    proof about plus

--- Combi.v
+++ Combi.v
@@ -53,12 +53,12 @@ Qed.
 
 Record ppack t := PPack { op : interp t -> nat -> interp t; plus_incr : forall x y k, x << y -> op x k << op y k}.
 
-Program Fixpoint plus_pack t : ppack t :=
+Program Fixpoint plust_pack t : ppack t :=
   match t as t return  ppack t with
   | Iota => PPack Iota (fun f k => f + k) _
   | Arrow t1 t2 => PPack
     (Arrow t1 t2)
-    (fun f k => exist _ (fun v => op t2 (plus_pack t2) (proj1_sig f v) k) _)
+    (fun f k => exist _ (fun v => op t2 (plust_pack t2) (proj1_sig f v) k) _)
     _
   end.
 
@@ -76,9 +76,49 @@ Obligation 3.
 fold interp.
 unfold cmp, ordsig, proj1_sig, cmp, ordfun in *.
 intros.
-destruct (plus_pack t2).
+destruct (plust_pack t2).
 unfold op.
 firstorder.
 Defined.
 
-Definition plus t : interp t -> nat -> interp t := op t (plus_pack t).
+Definition plust {t} : interp t -> nat -> interp t := op t (plust_pack t).
+
+Infix "+_" := plust (at level 50, no associativity).
+
+(* Use equality of nat on Iota, and functional extensionality otherwise *)
+Fixpoint eq {t} : interp t -> interp t -> Prop :=
+  match t with
+  | Iota => fun x y => x = y
+  | Arrow t1 t2 => fun f g => forall x, eq (proj1_sig f x) (proj1_sig g x)
+  end.
+
+Infix "==" := eq (at level 60, no associativity).
+
+Lemma plus_0 t : forall (v:interp t), v +_ 0 == v.
+Proof.
+  intros.
+  induction t; simpl; fold interp.
+  - easy.
+  - intros.
+    apply IHt2.
+Qed.
+
+Lemma plus_assoc t : forall (v:interp t) k k', (v +_ k) +_ k' == v +_ (k + k').
+Proof.
+  intros.
+  induction t; simpl; fold interp.
+  - symmetry.
+    apply plus_assoc.
+  - intros. apply IHt2.
+Qed.
+
+Lemma plus_monotonic t : forall (v:interp t) k k', k < k' -> v +_ k << v +_ k'.
+Proof.
+  intros.
+  induction t; simpl; fold interp; unfold cmp.
+  - unfold ordnat.
+    intuition.
+  - unfold ordsig, cmp, ordfun.
+    intros.
+    apply IHt2.
+Qed.
